22146f996b3a1316152beef7479b0b647acf4024
ivan <ix221@nyu.edu>
2023-11-01 13:17:19 -0400

    added app.mjs

--- app.mjs
+++ app.mjs
@@ -1,9 +1,18 @@
-import express from 'express'
-import path from 'path'
-import { fileURLToPath } from 'url';
+import './config.mjs';
+import express from 'express';
+import './db.mjs';
+import mongoose from 'mongoose';
+
+const user = mongoose.model('user');
 
 const app = express();
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
-app.listen(process.env.PORT || 3000);
+// set up express static
+import url from 'url';
+import path from 'path';
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// configure templating to hbs
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
---
+++ config.mjs
@@ -0,0 +1,4 @@
+import { config } from 'dotenv';
+
+// Use dotenv.config() to load environment variables
+config();
