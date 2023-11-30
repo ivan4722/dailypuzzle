1ce9aeff42c01d457488fec3059c1bb2068e54c6
ivan <ix221@nyu.edu>
2023-11-30 16:56:25 -0500

    fixed shell script

--- start_app.sh
+++ start_app.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-# Load necessary modules and set up the environment
-module load gcc-11.2  # Add other module load commands as needed
+module load gcc-11.2  
 
-# Navigate to your application directory
+
 /usr/local/lib/node_modules/pm2/bin/pm2 reload all
 
 
