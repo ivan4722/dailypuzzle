de83cc2283fbd893f5e0c08c4bd26d65190d209e
ivan4722 <136027956+ivan4722@users.noreply.github.com>
2024-03-05 14:11:37 -0500

    Update app.mjs

--- app.mjs
+++ app.mjs
@@ -22,83 +22,44 @@ const questions = new Map();
 //questions from Introduction to Counting & Probability by David Patrick
 //and https://www.northsouth.org/app11/docs/coaching/docs/Solutions%20to%20Pre-Test_AMC_AIME%20PrepClub.pdf
 
-questions.set('113023', "In how many ways can a President and a Vice-President be chosen from a group of 4 people? (assuming that the president and the vice president cannot be the same person)");
-questions.set('120123', "Coach is preparing a 5 person starting line up for his team. There are 12 total players, and 2 all star players from last year Chris and Andrew are guaranteed a spot on the team. How many starting lineups are possible?");
-questions.set('120223', "15 students in a chemistry class have lab every week. Each week, they are randomly divided into 5 groups of 3 students each. What is the probability that 3 students, Bob, Andrew, and Sally are in the same lab group this week? Enter your answer as a simplfied fraction.");
-questions.set('120323', "My friend and I are hoping to meet for lunch. We each arrive at the restaurant at a random time between 12pm and 1pm, stay for 15 minutes, and leave (if we don't see each other). What is the probability we will meet each other while at the restaurant? For example, If I show up at 12:10 and my friend shows up at 12:19, we will meet. However, if I arrive at 12:50 and my friend shows up at 12:20, we will not meet. Enter your answer as a simplified fraction.");
-questions.set('120423', "What is the probability that a random rearrangement of the letters in the word \"MATHEMATICS\" will begin with the letters \"MATH\"? Enter your answer as a simplified fraction.");
-questions.set('120523', "A bag has 4 red and 6 blue marbles. A marble is selected and not replaced, then a second is selected. What is the probability that both are the same color? Enter your answer as a simplified fraction.");
-questions.set('120623', "6 points are placed evenly around a circle labeled A,B,C,D,E, and F at random. What is the probability that two triangles ABC and DEF do not overlap? Enter your answer as a simplified fraction.");
-questions.set('120723', "How many distinct arrangements are there for the word TATTER?");
-questions.set('120823', "How many distinct arrangements are there for the word PAPA?");
-questions.set('120923', "How many 4-digit numbers have only odd digits?");
-questions.set('121023', "A certain two-digit number is equal to twice the sum of its digits. What is the product of its digits?");
-
-//repeating the questions until end of semester for grading
-
-questions.set('121123', "In how many ways can a President and a Vice-President be chosen from a group of 4 people? (assuming that the president and the vice president cannot be the same person)");
-questions.set('121223', "Coach is preparing a 5 person starting line up for his team. There are 12 total players, and 2 all-star players from last year, Chris and Andrew, are guaranteed a spot on the team. How many starting lineups are possible?");
-questions.set('121323', "15 students in a chemistry class have lab every week. Each week, they are randomly divided into 5 groups of 3 students each. What is the probability that 3 students, Bob, Andrew, and Sally, are in the same lab group this week? Enter your answer as a simplified fraction.");
-questions.set('121423', "My friend and I are hoping to meet for lunch. We each arrive at the restaurant at a random time between 12 pm and 1 pm, stay for 15 minutes, and leave (if we don't see each other). What is the probability we will meet each other while at the restaurant? For example, if I show up at 12:10 and my friend shows up at 12:19, we will meet. However, if I arrive at 12:50 and my friend shows up at 12:20, we will not meet. Enter your answer as a simplified fraction.");
-questions.set('121523', "What is the probability that a random rearrangement of the letters in the word 'MATHEMATICS' will begin with the letters 'MATH'? Enter your answer as a simplified fraction.");
-questions.set('121623', "A bag has 4 red and 6 blue marbles. A marble is selected and not replaced, then a second is selected. What is the probability that both are the same color? Enter your answer as a simplified fraction.");
-questions.set('121723', "6 points are placed evenly around a circle labeled A, B, C, D, E, and F at random. What is the probability that two triangles ABC and DEF do not overlap? Enter your answer as a simplified fraction.");
-questions.set('121823', "How many distinct arrangements are there for the word 'TATTER'?");
-questions.set('121923', "How many distinct arrangements are there for the word 'PAPA'?");
-questions.set('122023', "How many 4-digit numbers have only odd digits?");
-questions.set('122123', "A certain two-digit number is equal to twice the sum of its digits. What is the product of its digits?");
-
-questions.set('122223', "In how many ways can a President and a Vice-President be chosen from a group of 4 people? (assuming that the president and the vice president cannot be the same person)");
-questions.set('122323', "Coach is preparing a 5 person starting line up for his team. There are 12 total players, and 2 all-star players from last year, Chris and Andrew, are guaranteed a spot on the team. How many starting lineups are possible?");
-questions.set('122423', "15 students in a chemistry class have lab every week. Each week, they are randomly divided into 5 groups of 3 students each. What is the probability that 3 students, Bob, Andrew, and Sally, are in the same lab group this week? Enter your answer as a simplified fraction.");
-questions.set('122523', "My friend and I are hoping to meet for lunch. We each arrive at the restaurant at a random time between 12 pm and 1 pm, stay for 15 minutes, and leave (if we don't see each other). What is the probability we will meet each other while at the restaurant? For example, if I show up at 12:10 and my friend shows up at 12:19, we will meet. However, if I arrive at 12:50 and my friend shows up at 12:20, we will not meet. Enter your answer as a simplified fraction.");
-questions.set('122623', "What is the probability that a random rearrangement of the letters in the word 'MATHEMATICS' will begin with the letters 'MATH'? Enter your answer as a simplified fraction.");
-questions.set('122723', "A bag has 4 red and 6 blue marbles. A marble is selected and not replaced, then a second is selected. What is the probability that both are the same color? Enter your answer as a simplified fraction.");
-questions.set('122823', "6 points are placed evenly around a circle labeled A, B, C, D, E, and F at random. What is the probability that two triangles ABC and DEF do not overlap? Enter your answer as a simplified fraction.");
-questions.set('122923', "How many distinct arrangements are there for the word 'TATTER'?");
-questions.set('123023', "How many distinct arrangements are there for the word 'PAPA'?");
-questions.set('123123', "How many 4-digit numbers have only odd digits?");
-questions.set('010124', "A certain two-digit number is equal to twice the sum of its digits. What is the product of its digits?");
-
-
+questions.set('030524', "In how many ways can a President and a Vice-President be chosen from a group of 4 people? (assuming that the president and the vice president cannot be the same person)");
+questions.set('030624', "Coach is preparing a 5 person starting line up for his team. There are 12 total players, and 2 all star players from last year Chris and Andrew are guaranteed a spot on the team. How many starting lineups are possible?");
+questions.set('030724', "15 students in a chemistry class have lab every week. Each week, they are randomly divided into 5 groups of 3 students each. What is the probability that 3 students, Bob, Andrew, and Sally are in the same lab group this week? Enter your answer as a simplfied fraction.");
+questions.set('030824', "My friend and I are hoping to meet for lunch. We each arrive at the restaurant at a random time between 12pm and 1pm, stay for 15 minutes, and leave (if we don't see each other). What is the probability we will meet each other while at the restaurant? For example, If I show up at 12:10 and my friend shows up at 12:19, we will meet. However, if I arrive at 12:50 and my friend shows up at 12:20, we will not meet. Enter your answer as a simplified fraction.");
+questions.set('030924', "What is the probability that a random rearrangement of the letters in the word \"MATHEMATICS\" will begin with the letters \"MATH\"? Enter your answer as a simplified fraction.");
+questions.set('031024', "A bag has 4 red and 6 blue marbles. A marble is selected and not replaced, then a second is selected. What is the probability that both are the same color? Enter your answer as a simplified fraction.");
+questions.set('031124', "6 points are placed evenly around a circle labeled A,B,C,D,E, and F at random. What is the probability that two triangles ABC and DEF do not overlap? Enter your answer as a simplified fraction.");
+questions.set('031224', "How many distinct arrangements are there for the word TATTER?");
+questions.set('031324', "How many distinct arrangements are there for the word PAPA?");
+questions.set('031424', "How many 4-digit numbers have only odd digits?");
+questions.set('031524', "A certain two-digit number is equal to twice the sum of its digits. What is the product of its digits?");
+questions.set('031624', "A police department in a small city consists of 10 officers. If the department policy is to have 5 of the officers patrolling the streets, 2 of the officers working full time at the station, and 3 of the officers on reserve at the station, how many different divisions of the 10 officers into the 3 groups are possible?");
+questions.set('031724', "Coach is preparing a 5 person starting line up for his team. There are 12 total players, and 2 all-star players from last year, Chris and Andrew, are guaranteed a spot on the team. How many starting lineups are possible?");
+questions.set('031824', "A committee of 3 is to be formed from a group of 20 people. How many different committees are possible?");
+questions.set('031924', "How many different 7-place license plates are possible if the first 3 places are to be occupied by letters and the final 4 by numbers?");
+questions.set('032024', "Suppose that we have 3 cards that are identical in form, except that both sides of the first card are colored red, both sides of the second card are colored black, and one side of the third card is colored red and the other side black. The 3 cards are mixed up in a hat, and 1 card is randomly selected and put down on the ground. If the upper side of the chosen card is colored red, what is the probability that the other side is colored black?");
+questions.set('032124', "A bin contains 3 types of disposable flashlights. The probability that a type 1 flashlight will give more than 100 hours of use is .7, with the corresponding probabilities for type 2 and type 3 flashlights being .4 and .3, respectively. Suppose that 20 percent of the flashlights in the bin are type 1, 30 percent are type 2, and 50 percent are type 3. What is the probability that a randomly chosen flashlight will give more than 100 hours of use? Enter a decimal.");
 
 const answers = new Map();
 
-answers.set('113023', "12");
-answers.set('120123', "120");
-answers.set('120223', "1/91");
-answers.set('120323', "7/16");
-answers.set('120423', "1/990");
-answers.set('120523', "7/15");
-answers.set('120623', "3/10");
-answers.set('120723', "120");
-answers.set('120823', "6");
-answers.set('120923', "625");
-answers.set('121023', "8");
+answers.set('030524', "12");
+answers.set('030624', "120");
+answers.set('030724', "1/91");
+answers.set('030824', "7/16");
+answers.set('030924', "1/990");
+answers.set('031024', "7/15");
+answers.set('031124', "3/10");
+answers.set('031224', "120");
+answers.set('031324', "6");
+answers.set('031424', "625");
+answers.set('031524', "8");
+answers.set('031624', "2520");
+answers.set('031724', "252");
+answers.set('031824', "1140");
+answers.set('031924', "175760000");
+answers.set('032024', "1/3");
+answers.set('032124', "0.41");
 
-answers.set('121123', "12");
-answers.set('121223', "120");
-answers.set('121323', "1/91");
-answers.set('121423', "7/16");
-answers.set('121523', "1/990");
-answers.set('121623', "7/15");
-answers.set('121723', "3/10");
-answers.set('121823', "120");
-answers.set('121923', "6");
-answers.set('122023', "625");
-answers.set('122123', "8");
-
-answers.set('122223', "12");
-answers.set('122323', "120");
-answers.set('122423', "1/91");
-answers.set('122523', "7/16");
-answers.set('122623', "1/990");
-answers.set('122723', "7/15");
-answers.set('122823', "3/10");
-answers.set('122923', "120");
-answers.set('123023', "6");
-answers.set('123123', "625");
-answers.set('010124', "8");
 
 
 // set up express static
